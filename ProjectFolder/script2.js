c865f4c3cd1229d773abe8f32d069370f183fd95
toufiqh123 <146899761+toufiqh123@users.noreply.github.com>
2023-10-12 16:50:14 -0400

    Added Interest Form for courses

--- ProjectFolder/script2.js
+++ ProjectFolder/script2.js
@@ -5,3 +5,12 @@ const nav = document.getElementById('nav')
 toggle.addEventListener('click', function(){
     nav.classList.toggle('active')
 })
+
+function showConfirmation() {
+    var confirmationMessage = document.getElementById("confirmationMessage");
+    confirmationMessage.innerHTML = "Your interests have been submitted";
+    setTimeout(function() {
+        confirmationMessage.innerHTML = ''; // Clear the message
+    }, 5000);
+    return false;
+}
